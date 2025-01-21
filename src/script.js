5210051176f3589bbaad41e543bb90568bca7da4
sureshjoshi88 <sj3795789@gmail.com>
2025-01-21 11:56:59 +0530

    add new java script

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function getvalue(){
+    let input = document.getElementById("input-value");
+    let button = document.getElementById("ad-button")
+    let value = input.value
+    let container = document.getElementById("main-conatiner")
+    let containerChild = document.createElement("div")
+    
+
+}
