c8bb6552790005ffe2dade5cda3ae91af0bcfa0e
sureshjoshi88 <sj3795789@gmail.com>
2025-01-21 12:07:45 +0530

    add new java script

--- src/script.js
+++ src/script.js
@@ -1,9 +1,23 @@
 function getvalue(){
     let input = document.getElementById("input-value");
-    let button = document.getElementById("ad-button")
     let value = input.value
     let container = document.getElementById("main-conatiner")
     let containerChild = document.createElement("div")
+    container.appendChild(containerChild)
+    let child1 = document.createElement("div")
+    let child2 = document.createElement("div")
+    let child3 = document.createElement("div")
+    containerChild.classList.add("flex")
+    let elements1 = document.createElement("li")
+    // elements1.style.listStyle = "decimal"
+    console.log(elements1);
     
+    child1.appendChild(elements1)
+    let num = document.getElementById("num")
+    elements1.innerText = 1
 
 }
+let button = document.getElementById("ad-button")
+button.addEventListener("click",()=>{
+    getvalue();
+})
