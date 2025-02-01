dec7296ebc99852efc175c02cb08dd2657742bd1
sureshjoshi88 <sj3795789@gmail.com>
2025-02-01 19:58:10 +0530

    add new id in api call

--- todo-list-api/script.js
+++ todo-list-api/script.js
@@ -7,11 +7,14 @@ function printvalue(){
 let div = document.createElement("div")
 
 let filtertodos = todos.map((value,key)=>{
+    let h1 = document.createElement("h1")
     let h3 = document.createElement("h3")
     let h4 = document.createElement("h4")
     let hr = document.createElement("hr")
+    h1.innerText = value.id
     h3.innerText = value.title
     h4.innerText = value.completed
+    div.appendChild(h1)
     div.appendChild(h3)
     div.appendChild(h4)
     div.appendChild(hr)
