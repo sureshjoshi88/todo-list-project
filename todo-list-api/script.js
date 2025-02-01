9e72288ea0f12b8e0bad4896ed89a69b8671ad6e
sureshjoshi88 <sj3795789@gmail.com>
2025-02-01 19:56:16 +0530

    add new element

--- todo-list-api/script.js
+++ todo-list-api/script.js
@@ -8,9 +8,12 @@ let div = document.createElement("div")
 
 let filtertodos = todos.map((value,key)=>{
     let h3 = document.createElement("h3")
+    let h4 = document.createElement("h4")
     let hr = document.createElement("hr")
     h3.innerText = value.title
+    h4.innerText = value.title
     div.appendChild(h3)
+    div.appendChild(h4)
     div.appendChild(hr)
 
 })
