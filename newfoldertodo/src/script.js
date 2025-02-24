b6ea55be442db1f88c4f677f8ffe53fec08283a0
sureshjoshi88 <sj3795789@gmail.com>
2025-02-24 09:59:40 +0530

    editing this code

--- newfoldertodo/src/script.js
+++ newfoldertodo/src/script.js
@@ -98,7 +98,7 @@ function getTodos(title = "") {
     .catch((error) => {
       let container = document.getElementById("main-container");
       container.innerHTML = innerHTML = "<h1>something went wrong please try again</h1>"
-
+      console.log(error)
     });
 }
 
